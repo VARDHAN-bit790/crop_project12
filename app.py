d0203fb81cc0771c9adbf83fa800b06b394bac78
VARDHAN-bit790 <vardhanvardhan278@gmail.com>
2025-10-30 18:38:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ def predict():
 
 import os
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+# python web server
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
 
